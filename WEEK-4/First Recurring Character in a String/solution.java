7bd2c16df45ab38cfa24d1787bcca4826c41d407
bhoomishivhare <152948254+bhoomishivhare@users.noreply.github.com>
2025-08-10 18:59:51 +0530

    Update solution.java

--- WEEK-4/First Recurring Character in a String/solution.java
+++ WEEK-4/First Recurring Character in a String/solution.java
@@ -5,40 +5,40 @@ public class FirstRecurringCharacter {
     // ---------------------------------------------------
     // Approach 1: Using HashSet (Optimal)
     // ---------------------------------------------------
-    public static Character firstRecurringUsingSet(String s) {
-        Set<Character> seen = new HashSet<>();
-        for (char ch : s.toCharArray()) {
-            if (seen.contains(ch)) {
-                return ch;
-            }
-            seen.add(ch);
-        }
-        return null;
-    }
+    // public static Character firstRecurringUsingSet(String s) {
+    //     Set<Character> seen = new HashSet<>();
+    //     for (char ch : s.toCharArray()) {
+    //         if (seen.contains(ch)) {
+    //             return ch;
+    //         }
+    //         seen.add(ch);
+    //     }
+    //     return null;
+    // }
 
     // ---------------------------------------------------
     // Approach 2: Using HashMap to track first and second occurrences
     // ---------------------------------------------------
-    public static Character firstRecurringUsingMap(String s) {
-        Map<Character, Integer> firstIndexMap = new HashMap<>();
-        int earliestSecondIndex = Integer.MAX_VALUE;
-        Character result = null;
+    // public static Character firstRecurringUsingMap(String s) {
+    //     Map<Character, Integer> firstIndexMap = new HashMap<>();
+    //     int earliestSecondIndex = Integer.MAX_VALUE;
+    //     Character result = null;
 
-        for (int i = 0; i < s.length(); i++) {
-            char ch = s.charAt(i);
-            if (!firstIndexMap.containsKey(ch)) {
-                firstIndexMap.put(ch, i);
-            } else {
-                // Second occurrence found
-                if (i < earliestSecondIndex) {
-                    earliestSecondIndex = i;
-                    result = ch;
-                }
-            }
-        }
+    //     for (int i = 0; i < s.length(); i++) {
+    //         char ch = s.charAt(i);
+    //         if (!firstIndexMap.containsKey(ch)) {
+    //             firstIndexMap.put(ch, i);
+    //         } else {
+    //             // Second occurrence found
+    //             if (i < earliestSecondIndex) {
+    //                 earliestSecondIndex = i;
+    //                 result = ch;
+    //             }
+    //         }
+    //     }
 
-        return result;
-    }
+    //     return result;
+    // }
 
     // ---------------------------------------------------
     // Approach 3: Using fixed size array for ASCII charset
